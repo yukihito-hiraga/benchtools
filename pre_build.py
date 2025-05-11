57951e086b40d83bb4e43d5ac62f905b3474197b
yukihito-hiraga <yukihito.hiraga@uec.ac.jp>
2025-05-11 16:33:40 +0900

    update

--- pre_build.py
+++ pre_build.py
@@ -7,6 +7,15 @@ except ImportError:
     from yaml import Loader, Dumper
 from pathlib import Path
 
+def compile_option(type):
+    if "x64":
+        return "-O3 -funroll-all-loops -march=native -DNDEBUG `pkg-config --cflags --libs glib-2.0` -Wno-psabi -Wno-unused-function -Wno-unused-result -Wno-stringop-overflow -Wno-implicit-function-declaration"
+    if "armlinux":
+        return "-O3 -Wno-psabi -Wno-unused-function -Wno-unused-result -funroll-all-loops -march=native -DNDEBUG -Wno-stringop-overflow `pkg-config --cflags --libs glib-2.0` -Wno-discarded-qualifiers"
+    if "asahi":
+        return "-O3 -Wno-psabi -Wno-unused-function -Wno-unused-result -funroll-all-loops -march=native -mcpu=native -DNDEBUG -Wno-stringop-overflow `gtk-config --cflags --libs`"
+    return "-O3 -funroll-all-loops -march=native -DNDEBUG `pkg-config --cflags --libs glib-2.0` -Wno-psabi -Wno-unused-function -Wno-unused-result -Wno-stringop-overflow -Wno-implicit-function-declaration"
+
 def main():        
     type = ""
     if detect()["cpu_type"] in ["AMD", "Intel"]:
@@ -20,6 +29,13 @@ def main():
     if not (impl_path/"common").exists():
         run(["git clone https://github.com/yukihito-hiraga/benchcommon.git"], shell=True, cwd = impl_path)
         run(["mv benchcommon common"], shell=True, cwd=impl_path)
+    with open(impl_path/"Makefile") as f:
+        lines = []
+        for l in f.readlines():
+            if "FLAG=" in l:
+                lines.append(f"FLAG={compile_option(type)}")
+            else:
+                lines.append(l)
     run([f"./preprocess.sh {type}"], shell=True, cwd=impl_path/"common")
 
 if __name__=="__main__":
